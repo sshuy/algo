44e3dd4df2a5807bc80cd4d13d80d4821d6c78fc
sshuy <sshuy@protonmail.ch>
2024-05-27 03:44:19 +0100

    fix: removedupe algo now works as expected

--- python/removeduplicate.py
+++ python/removeduplicate.py
@@ -2,9 +2,7 @@ def remove_duplicate(nums []) -> int:
     hash_set = set()
 
     for num in nums:
-        if nums in hash_set:
+        if num in hash_set:
             return True
-        else:
-            hash_set.add(nums)
-
+        hash_set.add(nums)
     return False
