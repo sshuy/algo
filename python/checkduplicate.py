50a4a99da327b76c4cf7e2557fc3fe9ba5e3ac67
sshuy <sshuy@protonmail.ch>
2024-05-27 05:34:31 +0100

    feat: added main func to check algo

--- python/checkduplicate.py
+++ python/checkduplicate.py
@@ -1,3 +1,13 @@
+def main():
+    ans = Solution()
+    dupes = [1, 2, 3, 3]
+    contains_dupe = ans.check_duplicate(dupes)
+
+    if contains_dupe:
+        print(f"stinky snake")
+    else:
+        print(f"falshy")
+
 class Solution:
     def check_duplicate(self, nums: list[int]) -> bool:
         hash_set = set()
@@ -5,5 +15,8 @@ class Solution:
         for num in nums:
             if num in hash_set:
                 return True
-            hash_set.add(nums)
+            hash_set.add(num)
         return False
+
+if __name__ == "__main__":
+    main()
