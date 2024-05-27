984a981de21bf7049bd17fc51f5a26cce9d97750
sshuy <sshuy@protonmail.ch>
2024-05-27 03:46:32 +0100

    fix: okay actually fixed the algorithm

--- python/removeduplicate.py
+++ python/removeduplicate.py
@@ -1,8 +1,9 @@
-def remove_duplicate(nums []) -> int:
-    hash_set = set()
+class Solution:
+    def check_duplicate(self, nums: List[int]) -> bool:
+        hash_set = set()
 
-    for num in nums:
-        if num in hash_set:
-            return True
-        hash_set.add(nums)
-    return False
+        for num in nums:
+            if num in hash_set:
+                return True
+            hash_set.add(nums)
+        return False
