574a8b387f69b910edaf1e8385add709722b0508
sshuy <sshuy@protonmail.ch>
2024-05-27 05:24:06 +0100

    fix: changed to true instead of creating an empty table

--- lua/checkduplicate.lua
+++ lua/checkduplicate.lua
@@ -4,13 +4,13 @@ function CheckDuplicates(nums)
 		if hash_set[number] then
 			return true
 		end
-		hash_set[number] = {}
+		hash_set[number] = true
 	end
 	return false
 end
 
-local dupes = { 1, 1, 2, 3 }
-local years = { 2011, 2012, 1990 }
+local dupes = { 1, 1, 2, 3 } -- prints 'stinky butt'
+local years = { 2011, 2012, 1990 } -- prints 'falshy'
 local containsDupe = CheckDuplicates(dupes)
 
 if containsDupe then
